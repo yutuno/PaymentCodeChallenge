380d6c82e523b85734a57dd751c9a90385182bae
yutuno <yuto_uno51@yahoo.co.jp>
2019-05-20 20:43:06 +0900

    UICollectionViewのExtensionを追加

--- PaymentCodeChallenge/Extension/UICollectionView+Extension.swift
+++ PaymentCodeChallenge/Extension/UICollectionView+Extension.swift
@@ -6,4 +6,60 @@
 //  Copyright © 2019 yuto. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UICollectionView {
+    func register(cellType: UICollectionViewCell.Type,
+                  bundle: Bundle? = nil) {
+        let className = cellType.className
+        let nib = UINib(nibName: className, bundle: bundle)
+        register(nib, forCellWithReuseIdentifier: className)
+    }
+    
+    func register(cellTypes: [UICollectionViewCell.Type],
+                  bundle: Bundle? = nil) {
+        cellTypes.forEach { register(cellType: $0, bundle: bundle) }
+    }
+    
+    func register(
+        reusableViewType: UICollectionReusableView.Type,
+        ofKind kind: String = UICollectionView.elementKindSectionHeader,
+        bundle: Bundle? = nil
+        ) {
+        let className = reusableViewType.className
+        let nib = UINib(nibName: className, bundle: bundle)
+        register(nib,
+                 forSupplementaryViewOfKind: kind,
+                 withReuseIdentifier: className)
+    }
+    
+    func register(
+        reusableViewTypes: [UICollectionReusableView.Type],
+        ofKind kind: String = UICollectionView.elementKindSectionHeader,
+        bundle: Bundle? = nil
+        ) {
+        reusableViewTypes.forEach {
+            register(reusableViewType: $0, ofKind: kind, bundle: bundle)
+        }
+    }
+    
+    func dequeueReusableCell<T: UICollectionViewCell>(
+        with type: T.Type,
+        for indexPath: IndexPath
+        ) -> T {
+        return dequeueReusableCell(
+            withReuseIdentifier: type.className,
+            for: indexPath) as! T
+    }
+    
+    func dequeueReusableView<T: UICollectionReusableView>(
+        with type: T.Type,
+        for indexPath: IndexPath,
+        ofKind kind: String = UICollectionView.elementKindSectionHeader
+        ) -> T {
+        return dequeueReusableSupplementaryView(
+            ofKind: kind,
+            withReuseIdentifier: type.className,
+            for: indexPath) as! T
+    }
+}
