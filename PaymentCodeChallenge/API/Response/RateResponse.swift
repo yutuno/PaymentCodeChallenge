faf381eb3f0d6897635533dec62a7fcc9499f016
yutuno <yuto_uno51@yahoo.co.jp>
2019-05-20 16:32:39 +0900

    QuotesをEntityに移行

--- PaymentCodeChallenge/API/Response/RateResponse.swift
+++ PaymentCodeChallenge/API/Response/RateResponse.swift
@@ -17,21 +17,3 @@ struct RateResponse: Decodable {
         case quotes
     }
 }
-
-struct Quotes: Codable {
-    let usd: Double
-    let jpy: Double
-    let eur: Double
-    let aud: Double
-    let gbp: Double
-    let pln: Double
-    
-    enum CodingKeys: String, CodingKey {
-        case usd = "USDUSD"
-        case jpy = "USDJPY"
-        case eur = "USDEUR"
-        case aud = "USDAUD"
-        case gbp = "USDGBP"
-        case pln = "USDPLN"
-    }
-}
---
+++ PaymentCodeChallenge/Entity/Quotes.swift
@@ -0,0 +1,27 @@
+//
+//  Quotes.swift
+//  PaymentCodeChallenge
+//
+//  Created by yuto on 2019/05/20.
+//  Copyright © 2019 yuto. All rights reserved.
+//
+
+import Foundation
+
+struct Quotes: Codable {
+    let usd: Double
+    let jpy: Double
+    let eur: Double
+    let aud: Double
+    let gbp: Double
+    let pln: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case usd = "USDUSD"
+        case jpy = "USDJPY"
+        case eur = "USDEUR"
+        case aud = "USDAUD"
+        case gbp = "USDGBP"
+        case pln = "USDPLN"
+    }
+}
