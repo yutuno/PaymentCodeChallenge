c7655bf2f08d846b0572efaf863635be18914d49
yutuno <yuto_uno51@yahoo.co.jp>
2019-05-20 13:22:42 +0900

    first commit

---
+++ PaymentCodeChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PaymentCodeChallenge
+//
+//  Created by yuto on 2019/05/20.
+//  Copyright © 2019 yuto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
